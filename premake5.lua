2352eca4038610f07c8dfcb5ae38a1d48fb10bb5
Liam Welch <liamw1@vt.edu>
2022-03-28 20:12:59 -0400

    Added installation scripts.  Removed premake directory and binary from repository

--- premake5.lua
+++ premake5.lua
@@ -15,17 +15,36 @@ workspace "Engine"
 	}
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+VULKAN_SDK = os.getenv("VULKAN_SDK")
 
 -- Include system directories relative to root folder (solution directory)
 IncludeDir = {}
 IncludeDir["GLFW"] = "%{wks.location}/Engine/lib/GLFW/include"
 IncludeDir["GLad"] = "%{wks.location}/Engine/lib/GLad/include"
+IncludeDir["shaderc"] = "%{wks.location}/Engine/lib/shaderc/include"
+IncludeDir["SPIRV_Cross"] = "%{wks.location}/Engine/lib/SPIRV-Cross"
+IncludeDir["VulkanSDK"] = "%{VULKAN_SDK}/Include"
 IncludeDir["ImGui"] = "%{wks.location}/Engine/lib/imgui"
 IncludeDir["glm"] = "%{wks.location}/Engine/lib/glm"
 IncludeDir["stb_image"] = "%{wks.location}/Engine/lib/stb_image"
 IncludeDir["spdlog"] = "%{wks.location}/Engine/lib/spdlog/include"
 IncludeDir["EnTT"] = "%{wks.location}/Engine/lib/EnTT/single_include"
 IncludeDir["llvm"] = "%{wks.location}/Game/lib/llvm/include"
+
+LibraryDir = {}
+LibraryDir["VulkanSDK"] = "%{VULKAN_SDK}/Lib"
+LibraryDir["VulkanSDK_Debug"] = "%{wks.location}/Engine/lib/VulkanSDK/Lib"
+
+Library = {}
+Library["Vulkan"] = "%{LibraryDir.VulkanSDK}/vulkan-1.lib"
+Library["VulkanUtils"] = "%{LibraryDir.VulkanSDK}/VkLayer_utils.lib"
+Library["ShaderC_Debug"] = "%{LibraryDir.VulkanSDK_Debug}/shaderc_sharedd.lib"
+Library["SPIRV_Cross_Debug"] = "%{LibraryDir.VulkanSDK_Debug}/spirv-cross-cored.lib"
+Library["SPIRV_Cross_GLSL_Debug"] = "%{LibraryDir.VulkanSDK_Debug}/spirv-cross-glsld.lib"
+Library["SPIRV_Tools_Debug"] = "%{LibraryDir.VulkanSDK_Debug}/SPIRV-Toolsd.lib"
+Library["ShaderC_Release"] = "%{LibraryDir.VulkanSDK}/shaderc_shared.lib"
+Library["SPIRV_Cross_Release"] = "%{LibraryDir.VulkanSDK}/spirv-cross-core.lib"
+Library["SPIRV_Cross_GLSL_Release"] = "%{LibraryDir.VulkanSDK}/spirv-cross-glsl.lib"
 
 
 
@@ -73,7 +92,8 @@ project "Engine"
 		"%{IncludeDir.ImGui}",
 		"%{IncludeDir.glm}",
 		"%{IncludeDir.stb_image}",
-		"%{IncludeDir.EnTT}"
+		"%{IncludeDir.EnTT}",
+		"%{IncludeDir.VulkanSDK}"
 	}
 
 	links
@@ -97,16 +117,34 @@ project "Engine"
 		defines "EN_DEBUG"
 		runtime "Debug"
 		symbols "on"
+		links
+		{
+			"%{Library.ShaderC_Debug}",
+			"%{Library.SPIRV_Cross_Debug}",
+			"%{Library.SPIRV_Cross_GLSL_Debug}"
+		}
 
 	filter "configurations:Release"
 		defines "EN_RELEASE"
 		runtime "Release"
 		optimize "on"
+		links
+		{
+			"%{Library.ShaderC_Release}",
+			"%{Library.SPIRV_Cross_Release}",
+			"%{Library.SPIRV_Cross_GLSL_Release}"
+		}
 
 	filter "configurations:Dist"
 		defines "EN_DIST"
 		runtime "Release"
 		optimize "on"
+		links
+		{
+			"%{Library.ShaderC_Release}",
+			"%{Library.SPIRV_Cross_Release}",
+			"%{Library.SPIRV_Cross_GLSL_Release}"
+		}
 
 
 
