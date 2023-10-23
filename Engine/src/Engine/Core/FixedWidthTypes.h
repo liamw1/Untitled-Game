3e2de95b574f7db7855bb31c8a16d279c1915349
Liam Welch <liamw1@vt.edu>
2023-10-22 22:14:54 -0400

    Merged changes from FundamentalTypes branch.

---
+++ Engine/src/Engine/Core/FixedWidthTypes.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Int.h"
+#include "Float.h"
+
+// Unsigned integer types
+using u8 = std::uint8_t;
+using u16 = std::uint16_t;
+using u32 = std::uint32_t;
+using u64 = std::uint64_t;
+using uSize = std::size_t;
+
+// Signed integer types
+using i8 = std::int8_t;
+using i16 = std::int16_t;
+using i32 = std::int32_t;
+using i64 = std::int64_t;
+using iSize = std::ptrdiff_t;
+
+// Floating point types    C++23: Can use standard fixed-width floating point types
+using f32 = float;
+using f64 = double;
